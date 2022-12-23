4e9a81fc729bde5618d128a49d942b1113efd948
“Paulo <paulo.nunes@live.de>
2022-12-23 19:57:05 -0300

    first commit

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    # Prod
-    bucket         = "paulonunes12dvptfstate001"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "paulonunes12dvptfstate001"
-  }
-}
